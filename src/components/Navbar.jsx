7a010e219e0ce6c614eb6152a92edfb3e1623a42
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-10-21 15:40:26 +0530

    fix : Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl text-[#00df9a] cursor-pointer">REACT.</h1>
+      <h1 className="w-full text-3xl text-[#00df9a] cursor-pointer">DATA.</h1>
       <ul className="hidden md:flex">
         <li className="p-4 cursor-pointer">Home</li>
         <li className="p-4 cursor-pointer">Company</li>
